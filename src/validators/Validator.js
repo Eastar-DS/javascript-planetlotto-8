cbb0952cc61365c0f32c9ee3fa2046043aa26622
Eastar-DS <72285245+Eastar-DS@users.noreply.github.com>
2026-01-10 14:40:25 +0900

    feat: Validator.js 추가 구현
    당첨 번호, 보너스번호 유효성 검사 추가

--- src/validators/Validator.js
+++ src/validators/Validator.js
@@ -4,6 +4,29 @@ class Validator {
       throw Error('구매 금액은 500원 단위여야 합니다.');
     }
   }
+
+  static validateWinningNumbers(winningNumbers) {
+    const winningNumbersSet = new Set(winningNumbers);
+    if (winningNumbers.length !== winningNumbersSet.size) {
+      throw Error('당첨 번호는 중복될 수 없습니다.');
+    }
+
+    if (winningNumbers.length !== 5) {
+      throw Error('당첨 번호는 5개가 입력되어야 합니다.');
+    }
+
+    winningNumbers.forEach((number) => {
+      if (number < 1 || number > 30) {
+        throw Error('당첨 번호는 1~30 사이의 숫자여야 합니다.');
+      }
+    });
+  }
+
+  static validateBonusNumbers(bonusNumber) {
+    if (bonusNumber < 1 || bonusNumber > 30) {
+      throw Error('당첨 번호는 1~30 사이의 숫자여야 합니다.');
+    }
+  }
 }
 
 export default Validator;
